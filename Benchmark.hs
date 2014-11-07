e5918f77cf83713fab79609dfe86d1ffbf989062
Oliver Charles <ollie@ocharles.org.uk>
2014-11-07 10:38:58 +0000

    A smaller benchmark

--- Benchmark.hs
+++ Benchmark.hs
@@ -3,61 +3,35 @@
 
 import Prelude hiding (div)
 
-import Data.Function
 import Control.Applicative
+import Data.Function
 import Data.List
-import System.Locale
-import GHCJS.Foreign
-import Data.Text (Text)
 import Data.Time
-import qualified Data.Text as T
-import HTML hiding (b)
+import GHCJS.Foreign
+import HTML
+import System.Locale
 
-data ClassType = ClassType
-  { classTypeName :: Text}
-  deriving (Show)
+import qualified Data.Text as T
 
 data Class = Class
   { classStartTime :: UTCTime
-  , classEndTime :: UTCTime
-  , classType :: ClassType
-  , classBusiness :: Business}
-  deriving (Show)
-
-classDuration :: Class -> NominalDiffTime
-classDuration Class{..} = classEndTime `diffUTCTime` classStartTime
-
-data Business = Business
-  { businessName :: Text
-  , businessPicture :: Maybe Text}
+  , classEndTime :: UTCTime}
   deriving (Show)
 
 main :: IO ()
 main = do
   c <- newTopLevelContainer
   renderTo c $
-    mkUI $ fmap mkClass [0 .. 3000]
+    mkUI $ fmap mkClass [0 .. 3000 :: Int]
 
   where mkClass n =
          Class { classStartTime = (fromIntegral $ n * 60 * 60) `addUTCTime` UTCTime (fromGregorian 2014 11 7) 0
-               , classEndTime = (fromIntegral $ (n + 1) * 60 * 60) `addUTCTime` UTCTime (fromGregorian 2014 11 7) 0
-               , classType = ClassType "Extreme Haskell Programming"
-               , classBusiness = Business "GHCJS Ltd." Nothing}
+               , classEndTime = (fromIntegral $ (n + 1) * 60 * 60) `addUTCTime` UTCTime (fromGregorian 2014 11 7) 0}
 
 mkUI :: [Class] -> HTML
-mkUI schedule =
-  div </> [
-   div </> [
-    case schedule of
-      [] -> emptySchedule
-      _ -> div </>
-           (dayRow <$> groupBy ((==) `on` (utctDay . classStartTime)) schedule)]]
+mkUI schedule = div </> dayRow <$> groupBy ((==) `on` (utctDay . classStartTime)) schedule
 
   where
-  emptySchedule =
-    div </> [
-     "There are no events here right now."]
-
   dayRow day =
     let dayHeader =
           row </> [
@@ -68,22 +42,8 @@ mkUI schedule =
           row </> [ classRow c ]
     in containerFluid </> (dayHeader : (clickableClassRow <$> day))
 
-classRow :: Class -> HTML
-classRow c =
-  let rowCell cellClass t =
-        div </> [text t]
-  in div </> [
-      container </> [
-       row </> [
-        rowCell "start-time" (toJSString $ T.pack (formatTime defaultTimeLocale "%H:%M" (classStartTime c))),
-        rowCell "duration" (toJSString $ T.pack (show (classDuration c)))]],
-      container </> [
-       row </> [
-        rowCell "type" (toJSString $ classTypeName (classType c)),
-        div </> [ showBusiness (classBusiness c) ]]]]
-
-  where
-  showBusiness b =
-    maybe (text (toJSString $ businessName b))
-          (\pic -> img)
-          (businessPicture b)
+  classRow c =
+    div </> [
+     container </> [
+      row </> [
+       div </> [ text $ toJSString $ T.pack (formatTime defaultTimeLocale "%H:%M" (classStartTime c))]]]]
--- HTML.hs
+++ HTML.hs
@@ -1,163 +1,63 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
-module HTML where
+module HTML (HTML, div, img, (</>), row, renderTo, newTopLevelContainer, text, container, containerFluid) where
 
-import Unsafe.Coerce
-import qualified Immutable
-import Prelude hiding (div, head, map, mapM, sequence, span)
-import Data.String (IsString(..))
-import Data.HashMap.Strict (HashMap, traverseWithKey)
-import Control.Lens hiding (aside, children, pre)
-import Data.Text (Text)
-import Data.Foldable (for_)
 import Control.Applicative
 import Control.Monad.IO.Class
-import Control.Monad.Trans.State.Strict
 import Data.IORef
-import Data.Traversable
+import Data.String (IsString(..))
 import GHCJS.DOM
 import GHCJS.DOM.Document
 import GHCJS.DOM.Element
 import GHCJS.DOM.Node
 import GHCJS.Foreign
 import GHCJS.Types
-import Reactive.Banana
-import Reactive.Banana.Frameworks
-import qualified Data.Text as T
+import Prelude hiding (div, head, map, mapM, sequence, span)
 import System.IO.Unsafe
-import GHCJS.Marshal
+import Unsafe.Coerce
 
-html, head, title, base, link, meta, style, script, noscript, body, section, nav, article, aside, h1, h2, h3, h4, h5, h6, hgroup, header, footer, address, p, hr, pre, blockquote, ol, ul, li, dl, dt, dd, figure, figcaption, div, a, em, strong, small, s, cite, q, dfn, abbr, data_, time, code, var, samp, kbd, sub, sup, i, b, u, mark, ruby, rt, rp, bdi, bdo, span, br, wbr, ins, del, img, iframe, embed, object, param, video, audio, source, track, canvas, map, area, table, caption, colgroup, col, tbody, thead, tfoot, tr, td, th, form, fieldset, legend, label, input, button, select, datalist, optgroup, option, textarea, keygen, output, progress, meter, details, summary, command, menu, dialog :: HTML
+--------------------------------------------------------------------------------
+data VNode
+newtype HTML = HTML (JSRef VNode)
 
-html = emptyElement "html"
-head = emptyElement "head"
-title = emptyElement "title"
-base = emptyElement "base"
-link = emptyElement "link"
-meta = emptyElement "meta"
-style = emptyElement "style"
-script = emptyElement "script"
-noscript = emptyElement "noscript"
-body = emptyElement "body"
-section = emptyElement "section"
-nav = emptyElement "nav"
-article = emptyElement "article"
-aside = emptyElement "aside"
-h1 = emptyElement "h1"
-h2 = emptyElement "h2"
-h3 = emptyElement "h3"
-h4 = emptyElement "h4"
-h5 = emptyElement "h5"
-h6 = emptyElement "h6"
-hgroup = emptyElement "hgroup"
-header = emptyElement "header"
-footer = emptyElement "footer"
-address = emptyElement "address"
-p = emptyElement "p"
-hr = emptyElement "hr"
-pre = emptyElement "pre"
-blockquote = emptyElement "blockquote"
-ol = emptyElement "ol"
-ul = emptyElement "ul"
-li = emptyElement "li"
-dl = emptyElement "dl"
-dt = emptyElement "dt"
-dd = emptyElement "dd"
-figure = emptyElement "figure"
-figcaption = emptyElement "figcaption"
+instance IsString HTML where
+  fromString = text . toJSString
+
+--------------------------------------------------------------------------------
+foreign import javascript safe
+  "console.time('new VText'); $r = new VText($1); console.timeEnd('new VText');" text :: JSString -> HTML
+
+foreign import javascript safe
+  "console.time('new VNode'); $r = new VNode($1); console.timeEnd('new VNode');" emptyElement :: JSString -> HTML
+
+foreign import javascript safe
+  "console.time('setVNodeChildren'); if ($1.type == 'VirtualNode') { $r = new VNode($1.tagName, $1.properties, $2); } else { $r = $1; } console.timeEnd('setVNodeChildren')" setVNodeChildren :: HTML -> JSArray a -> HTML
+
+--------------------------------------------------------------------------------
+infixl 1 </>
+(</>) :: HTML -> [HTML] -> HTML
+n </> xs = setVNodeChildren n (unsafePerformIO (toArray (unsafeCoerce xs)))
+
+--------------------------------------------------------------------------------
+div, img, row, containerFluid, container :: HTML
 div = emptyElement "div"
-a = emptyElement "a"
-em = emptyElement "em"
-strong = emptyElement "strong"
-small = emptyElement "small"
-s = emptyElement "s"
-cite = emptyElement "cite"
-q = emptyElement "q"
-dfn = emptyElement "dfn"
-abbr = emptyElement "abbr"
-data_ = emptyElement "data"
-time = emptyElement "time"
-code = emptyElement "code"
-var = emptyElement "var"
-samp = emptyElement "samp"
-kbd = emptyElement "kbd"
-sub = emptyElement "sub"
-sup = emptyElement "sup"
-i = emptyElement "i"
-b = emptyElement "b"
-u = emptyElement "u"
-mark = emptyElement "mark"
-ruby = emptyElement "ruby"
-rt = emptyElement "rt"
-rp = emptyElement "rp"
-bdi = emptyElement "bdi"
-bdo = emptyElement "bdo"
-span = emptyElement "span"
-br = emptyElement "br"
-wbr = emptyElement "wbr"
-ins = emptyElement "ins"
-del = emptyElement "del"
-img = emptyElement "img"
-iframe = emptyElement "iframe"
-embed = emptyElement "embed"
-object = emptyElement "object"
-param = emptyElement "param"
-video = emptyElement "video"
-audio = emptyElement "audio"
-source = emptyElement "source"
-track = emptyElement "track"
-canvas = emptyElement "canvas"
-map = emptyElement "map"
-area = emptyElement "area"
-table = emptyElement "table"
-caption = emptyElement "caption"
-colgroup = emptyElement "colgroup"
-col = emptyElement "col"
-tbody = emptyElement "tbody"
-thead = emptyElement "thead"
-tfoot = emptyElement "tfoot"
-tr = emptyElement "tr"
-td = emptyElement "td"
-th = emptyElement "th"
-form = emptyElement "form"
-fieldset = emptyElement "fieldset"
-legend = emptyElement "legend"
-label = emptyElement "label"
-input = emptyElement "input"
-button = emptyElement "button"
-select = emptyElement "select"
-datalist = emptyElement "datalist"
-optgroup = emptyElement "optgroup"
-option = emptyElement "option"
-textarea = emptyElement "textarea"
-keygen = emptyElement "keygen"
-output = emptyElement "output"
-progress = emptyElement "progress"
-meter = emptyElement "meter"
-details = emptyElement "details"
-summary = emptyElement "summary"
-command = emptyElement "command"
-menu = emptyElement "menu"
-dialog = emptyElement "dialog"
+img = emptyElement "div"
+row = div
+containerFluid = div
+container = div
 
-foreign import javascript unsafe
-  "console.time($1)" timeStart :: JSString -> IO ()
-
-foreign import javascript unsafe
-  "console.timeEnd($1)" timeEnd :: JSString -> IO ()
-
-timing :: (Applicative m, MonadIO m) => JSString -> m a -> m a
-timing lbl m = liftIO (timeStart lbl) *> m <* liftIO (timeEnd lbl)
-
+--------------------------------------------------------------------------------
 foreign import javascript unsafe
   "vdom($1)"
   createElement :: HTML -> IO Element
 
+--------------------------------------------------------------------------------
 data Diff
 foreign import javascript unsafe
   "console.time('ffi:diff'); $r = window.virtualDom.diff($1, $2); console.timeEnd('ffi:diff');"
   diff :: HTML -> HTML -> IO (JSRef Diff)
 
+--------------------------------------------------------------------------------
 foreign import javascript unsafe
   "window.virtualDom.patch($1, $2)"
   patch :: Element -> JSRef Diff -> IO ()
@@ -166,6 +66,7 @@ foreign import javascript unsafe
 -- An element in the DOM that we can render virtualdom elements to
 data VNodePresentation = VNodePresentation (IORef HTML) Element
 
+--------------------------------------------------------------------------------
 -- Render our internal HTML tree representation into a VNode. We first
 -- convert our HTML into a VTree, and then diff this against the container
 -- and apply the resulting updates.
@@ -177,11 +78,10 @@ renderTo (VNodePresentation ioref el) !e = timing "renderTo" $ do
   timing "patch" (patch el patches)
   writeIORef ioref e
 
+--------------------------------------------------------------------------------
 newTopLevelContainer :: IO VNodePresentation
 newTopLevelContainer = do
-  emptyObj <- newObj
-  -- setProp ("key" :: JSString) ("0" :: JSString) emptyObj
-  initialVNode <- return div
+  let initialVNode = div
   currentVNode <- newIORef initialVNode
   el <- createElement initialVNode
   Just doc <- currentDocument
@@ -189,43 +89,12 @@ newTopLevelContainer = do
   _ <- nodeAppendChild bodyNode (Just el)
   return (VNodePresentation currentVNode el)
 
+--------------------------------------------------------------------------------
+foreign import javascript unsafe
+  "console.time($1)" timeStart :: JSString -> IO ()
 
-data VNode
+foreign import javascript unsafe
+  "console.timeEnd($1)" timeEnd :: JSString -> IO ()
 
-newtype HTML = HTML (JSRef VNode)
-
-foreign import javascript safe
-  "console.time('new VText'); $r = new VText($1); console.timeEnd('new VText');" text :: JSString -> HTML
-
-foreign import javascript safe
-  "new VNode($1)" emptyElement :: JSString -> HTML
-
-foreign import javascript safe
-  "$1 && $1.type == 'VirtualNode'" isVNode :: HTML -> JSBool
-
-foreign import javascript safe
-  "console.time('setVNodeChildren'); if ($1.type == 'VirtualNode') { $r = new VNode($1.tagName, $1.properties, $2); } else { $r = $1; } console.timeEnd('setVNodeChildren')" setVNodeChildren :: HTML -> JSArray a -> HTML
-
-infixl 1 </>
-(</>) :: HTML -> [HTML] -> HTML
-n </> xs = setVNodeChildren n (unsafePerformIO (toArray (unsafeCoerce xs)))
-
-instance IsString HTML where
-  fromString = text . toJSString
-
-foreign import javascript safe
-  "console.time('vNodeGetAttributes'); $r = Immutable.Map($1.properties.attributes); console.timeEnd('vNodeGetAttributes');"
-  vNodeGetAttributes :: HTML -> Immutable.Map
-
-foreign import javascript safe
-  "console.time('vNodeSetAttributes'); $r = new VNode($1.tagName, {'attributes': $2.toJS()}, $1.children); console.timeEnd('vNodeSetAttributes');"
-  vNodeSetAttributes :: HTML -> Immutable.Map -> HTML
-
-row :: HTML
-row = div
-
-containerFluid :: HTML
-containerFluid = div
-
-container :: HTML
-container = div
+timing :: (Applicative m, MonadIO m) => JSString -> m a -> m a
+timing lbl m = liftIO (timeStart lbl) *> m <* liftIO (timeEnd lbl)
